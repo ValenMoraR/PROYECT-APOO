88a43860eee724ac68c4a3aa969cd8eb761f95f2
Valentina Morales <111003190+ValenMoraR@users.noreply.github.com>
2023-11-06 21:32:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import os
 from code_1 import tk, Reproductor
 
 ventana = tk.Tk()
-ventana.geometry("620x300")
-ventana.resizable(width=0, height=0)
+ventana.geometry("800x300")
+# ventana.resizable(width=0, height=0)
+if os.path.exists("favoritos.json"):
+    os.remove("favoritos.json")
 
 reproductor = Reproductor(ventana)
-reproductor.cargar_favoritos()
 if __name__ == "__main__": 
     ventana.mainloop()
