fff6b645b843ced21635d82df248410c9a402336
Valentina Morales <111003190+ValenMoraR@users.noreply.github.com>
2023-11-06 21:33:00 -0500

    Update cancion.py

--- cancion.py
+++ cancion.py
@@ -3,3 +3,11 @@ class Cancion:
         self.nombre = nombre
         self.archivo = archivo
         self.es_favorita = False
+    
+    def to_dict(self):
+        """Convierte una instancia de Cancion en un diccionario serializable en JSON."""
+        return {
+            "nombre": self.nombre,
+            "archivo": self.archivo,
+            "es_favorita": self.es_favorita
+        }
